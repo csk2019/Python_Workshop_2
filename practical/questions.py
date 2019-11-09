f83047ceec6ab6d522105c6f0cf48d2674e9cc7b
unknown <cecimari80@gmail.com>
2019-11-09 11:54:23 +0300

    Updated Questionsr

---
+++ practical/questions.py
@@ -0,0 +1,36 @@
+#Remember to add a # to the question after you answer the question
+1. Write a print statement with your name and your github account
+
+
+2. Create two strings to represent your name and your github account
+
+
+
+3. Define a function that prints the name and github account you defined in 2 above. Taking the two as arguments. Remember to use
+    string formating for the print statement
+
+
+
+
+4.  Define another function that takes two integers as argumnents. Using the function, calculate the summation,product, division
+    subtraction,modulus and exponentiation of the numbers. Append each of the calculation to a list and use a return statement to 
+    return the list
+
+
+
+
+5. Define another function that takes a list as an argument. Pass to it  the returned list in 4 above, use a for loop to traverse 
+the list, and print the individual calculations you returned in (4) above
+
+
+
+
+6. Lastly, define a function that accepts age from a user. use an if statement to check whether the age is above 18.
+  -- If the age is above 18, ask the user for their name and their github account name and print them using string formatting
+  --if the age is above 25,call the function in (3)
+  -- if the age is above 35 call the function in (4), 
+  --if the age is above 55 call the function in(5)
+  -- else create a variable and assign it the string "this is the end of todays practical" and then print the upper case version of
+  the string
+
+
